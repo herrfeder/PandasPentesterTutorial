e28ade60f15d721b297cdb93c4bd46a6c1445be1
herrfeder <d.lassig@t-online.de>
2019-05-27 10:37:04 +0000

    Subject: adding data, datasupport TODO: interactive update of widgets

---
+++ widgetslib/exceptions.py
@@ -0,0 +1,22 @@
+class Error(Exception):
+    '''
+    Base class for other Exceptions
+    '''
+    pass
+
+class WrongProjectEnvironment(Error):
+    pass
+
+class WrongDomainSyntax(Error):
+    #print("You need to use at least this syntax. Example: http://url.com")
+    pass
+
+class DomainNoIp(Error):
+    #print("This domain doesn't have an IP address, maybe typo?")
+    pass
+
+class NoScanAvailable(Error):
+    pass
+
+class WrongDataFrameSize(Error):
+    pass
